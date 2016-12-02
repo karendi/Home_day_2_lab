8ecbf1123368445083a2c77b0ffe0b131c811ca5
Sharonnjihia <sharonkarendi16@gmail.com>
2016-12-02 10:14:12 +0300

    Fixed the error for returning numbers

--- word_count.py
+++ word_count.py
@@ -3,7 +3,14 @@ def words(statement):
   S = statement.split()
   for word in S:
     if word not in results:
-      results[word] = 1
+        if word.isdigit():
+            results[int(word)] = 1
+        else:
+            results[word] = 1
+
     else:
-      results[word] += 1
+        if word.isdigit():
+            results[int(word)] += 1
+        else:
+            results[word] += 1
   return results
